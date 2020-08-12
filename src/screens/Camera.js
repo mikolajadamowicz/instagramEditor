c4c60bbb0a157e2b035a046f5dae710f7099e603
Mikolaj <mikolaj.adamowicz@acaisoft.com>
2020-08-12 21:17:14 +0200

    fix: fixed react-native-photo-editor pods bug

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useRef } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
 const Camera = () => {
+  const camera = useRef();
   const takePicture = async () => {
     if (this.camera) {
       const options = { quality: 0.5, base64: true };
@@ -10,16 +17,13 @@ const Camera = () => {
       console.log(data.uri);
     }
   };
-
+  // type={RNCamera.Constants.Type.back}
+  // flashMode={RNCamera.Constants.FlashMode.on}
   return (
-    <View style={styles.container}>
+    <>
       <RNCamera
-        ref={(ref) => {
-          this.camera = ref;
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
+        ref={camera}
+        style={styles.container}
         androidCameraPermissionOptions={{
           title: 'Permission to use camera',
           message: 'We need your permission to use your camera',
@@ -32,40 +36,23 @@ const Camera = () => {
           buttonPositive: 'Ok',
           buttonNegative: 'Cancel',
         }}
-        onGoogleVisionBarcodesDetected={({ barcodes }) => {
-          console.log(barcodes);
-        }}
       />
-      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-        <TouchableOpacity
-          onPress={this.takePicture.bind(this)}
-          style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SNAP </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+      <TouchableOpacity onPress={takePicture} style={styles.capture}>
+        <Text style={{ fontSize: 14 }}> SNAP </Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
   },
   capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
+    position: 'absolute',
+    width: '10%',
+    height: '10%',
+    backgroundColor: 'blue',
   },
 });
 
